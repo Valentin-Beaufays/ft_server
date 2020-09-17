63feeaaf9a8601df5901bfdb37e50ea01d0b2448
Valentin Beaufays <beaufaysvalentin@gmail.com>
2020-09-17 17:27:06 +0200

    finished

--- srcs/start.sh
+++ srcs/start.sh
@@ -3,7 +3,7 @@
 ./etc/init.d/mysql start
 
 #setup phpMyAdmin
-mysql < /var/www/phpMyAdmin/sql/create_tables.sql -u root
+mysql < /var/www/wordpress/phpmyadmin/sql/create_tables.sql -u root
 echo "GRANT ALL PRIVILEGES ON phpmyadmin.* TO 'pma'@'localhost' IDENTIFIED BY 'pmapass';" | mysql -u root
 echo "FLUSH PRIVILEGES;" | mysql -u root
 
@@ -11,6 +11,9 @@ echo "FLUSH PRIVILEGES;" | mysql -u root
 echo "CREATE DATABASE wordpress;" | mysql -u root
 echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'admin'@'localhost' IDENTIFIED BY 'admin';" | mysql -u root
 echo "FLUSH PRIVILEGES;" | mysql -u root
+
+#setup root user
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'rootpass';" | mysql -u root
 
 #start services
 ./etc/init.d/php7.3-fpm start
